23a6075d98d99ee1b60f8cdc3b18b18f2dcb4683
Alessandro <106855795+aless-x-code@users.noreply.github.com>
2023-08-11 11:59:53 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,17 +10,17 @@ int main(int argc, char *const argv[])
     fibo_index = 3; // (counting the first index as 1) we initialize our sequence at index 3
     fibo_index_value = 2; // correspondingly, we initialize index value as 2
     
-    index_1 = 1; // holds the before-last fibonacci number
-    index_2 = 1; // hold the current fibonacci number
+    index_1 = 1; // hold before-last fibonacci number
+    index_2 = 1; // hold current fibonacci number
     
     cout << "Enter the seeked position value from the fibonacci sequence: ";
     cin >> seeked_index;
     
-    // fail safe
-    if (seeked_index == 1) fibo_index_value = 1;
+    if (seeked_index == 0) fibo_index_value = 0; // prevent loop from running if seeked index is 0 or 2
+    if (seeked_index == 2) fibo_index_value = 1;
+        
     
-    // while the position is lower than position given..
-    for ( ; fibo_index < seeked_index; fibo_index++)
+    for ( ; fibo_index < seeked_index; fibo_index++) // while fibo index is lower than the seeked index, increase index by 1
     {
         // fibonacci logic
         index_1 = index_2; // shift index_1 forward to become the before-last leading number (aka fibonacci)
@@ -29,5 +29,7 @@ int main(int argc, char *const argv[])
 
     }
     cout << "The value of position #" << seeked_index << " is: " << fibo_index_value << endl;
+    
+    return 0;
 }
 
