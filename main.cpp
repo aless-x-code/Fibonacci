603412e79e63c3c33fff1f38c071bc11be07970a
Alessandro <106855795+aless-x-code@users.noreply.github.com>
2023-08-15 20:35:16 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, char *const argv[])
 {
-    long seeked_index, fibo_index, fibo_index_value, index_1, index_2;
+    long desired_index, fibo_index, fibo_index_value, index_1, index_2;
     
     fibo_index = 3; // (counting the first index as 1) we initialize our sequence at index 3
     fibo_index_value = 2; // correspondingly, we initialize index value as 2
@@ -13,14 +13,14 @@ int main(int argc, char *const argv[])
     index_1 = 1; // hold before-last fibonacci number
     index_2 = 1; // hold current fibonacci number
     
-    cout << "Enter the seeked position value from the fibonacci sequence: ";
-    cin >> seeked_index;
+    cout << "Enter the desired position value from the fibonacci sequence: ";
+    cin >> desired_index;
     
-    if (seeked_index == 0) fibo_index_value = 0; // prevent loop from running if seeked index is 0 or 2
-    if (seeked_index == 2) fibo_index_value = 1;
+    if (desired_index == 0) fibo_index_value = 0; // prevent loop from running if desired index is 0 or 2
+    if (desired_index == 2) fibo_index_value = 1;
         
     
-    for ( ; fibo_index < seeked_index; fibo_index++) // while fibo index is lower than the seeked index, increase index by 1
+    for ( ; fibo_index < desired_index; fibo_index++) // while fibo index is lower than the desired index, increase index by 1
     {
         // fibonacci logic
         index_1 = index_2; // shift index_1 forward to become the before-last leading number (aka fibonacci)
@@ -28,7 +28,7 @@ int main(int argc, char *const argv[])
         fibo_index_value = index_1 + index_2; // sum the before-last leading number, and leading number, to get the next leading number (aka fibonacci)
 
     }
-    cout << "The value of position #" << seeked_index << " is: " << fibo_index_value << endl;
+    cout << "The value of position #" << desired_index << " is: " << fibo_index_value << endl;
     
     return 0;
 }
